7dc22bcbe5d5295e5ece739a89b22d63b9392f72
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-08-16 17:06:54 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,2 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+na='http://newsonair.com/RNU-NSD-Audio-Archive-Search.aspx'
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+driver = webdriver.Chrome(chrome_options=options)
+driver.get(na)
